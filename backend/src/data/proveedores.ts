a6cbbd330f1b88af509c40b20986cd5a416074a3
P4ul4f <paulaferreyra24@gmail.com>
2025-05-27 17:09:46 -0300

    db conexión

---
+++ backend/src/data/proveedores.ts
@@ -0,0 +1,14 @@
+export const proveedoresRandom = [
+    {
+      nombre: 'Proveedora Alfa',
+      telefono: '341-5551234',
+      email: 'contacto@proveedoraalfa.com',
+      estado: true,
+    },
+    {
+      nombre: 'Distribuciones Beta',
+      telefono: '351-4225678',
+      email: 'ventas@distribucionesbeta.com',
+      estado: true,
+    },
+];

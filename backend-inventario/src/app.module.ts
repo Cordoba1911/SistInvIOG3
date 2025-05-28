4584f13a12ec10fa557e1ea23b0e6f374eda8a74
Cordoba1911 <sruizcucchiarelli@gmail.com>
2025-05-28 20:37:00 -0300

    migrado a nest.js

---
+++ backend-inventario/src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { ArticulosModule } from './articulos/articulos.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+@Module({
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    username: 'root',
+    password: 'root',
+    host: 'localhost',
+    port: 3306,
+    database: 'inventario',
+    entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    synchronize: true
+  }), ArticulosModule],
+})
+export class AppModule { }

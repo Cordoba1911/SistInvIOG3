8395aedc720610ac076cc2def6a50d0c9b66c24e
P4ul4f <paulaferreyra24@gmail.com>
2025-06-02 20:41:47 -0300

    orden compra funciones sin relaciones

--- backend-inventario/src/app.module.ts
+++ backend-inventario/src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ArticulosModule } from './articulos/articulos.module';
 import { ProveedorModule } from './proveedores/proveedor.module';
+import { OrdenCompraModule } from './orden_compra/orden-compra.module';
 
 @Module({
   imports: [
@@ -24,6 +25,7 @@ import { ProveedorModule } from './proveedores/proveedor.module';
     }),
     ArticulosModule,
     ProveedorModule,
+    OrdenCompraModule,
   ],
 })
 export class AppModule {}

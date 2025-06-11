aa5faf9294a60ea17d442b567b667e48ace8e35a
P4ul4f <paulaferreyra24@gmail.com>
2025-06-11 20:15:07 -0300

    Ventas

---
+++ backend-inventario/src/ventas/ventas.entity.ts
@@ -0,0 +1,13 @@
+// import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+// @Entity({ name: 'Ventas' })
+// export class Venta {
+//   @PrimaryGeneratedColumn()
+//   id: number;
+
+//   @Column({ nullable: false })
+//   cantidad: number;
+
+//   @Column({ nullable: true, type: 'datetime' })
+//   fecha_venta: Date;
+// }

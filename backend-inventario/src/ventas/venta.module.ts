9584e7a08289ed79fbec15b7e0ea47723b5c45c7
P4ul4f <paulaferreyra24@gmail.com>
2025-06-12 19:46:04 -0300

    ventas create

--- backend-inventario/src/ventas/venta.module.ts
+++ backend-inventario/src/ventas/venta.module.ts
@@ -5,9 +5,13 @@ import { VentaService } from './venta.service';
 import { Venta } from './venta.entity';
 import { Articulo } from 'src/articulos/articulo.entity';
 import { OrdenCompraModule } from 'src/orden_compra/orden-compra.module';
+import { DetalleVenta } from './detalle-venta.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Venta, Articulo]), OrdenCompraModule],
+  imports: [
+    TypeOrmModule.forFeature([Venta, Articulo, DetalleVenta]),
+    OrdenCompraModule,
+  ],
   controllers: [VentaController],
   providers: [VentaService],
   exports: [VentaService],
